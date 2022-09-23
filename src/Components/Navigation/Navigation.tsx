32c9ef3c2bea76e5d019fdfa869f814ef9deb1c4
DenisMiso4enko <derrmisochenko17@gmail.com>
2022-09-23 15:23:12 +0300

    test

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -5,7 +5,7 @@ const Navigation = () => {
     return (
         <nav className="h-[50px] flex justify-between px-5 bg-gray-500 text-white items-center">
             <span>React 2022</span>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, perspiciatis?</p>
+
             <span>
                 <Link className="mr-2" to="/">Products</Link>
                 <Link to="/about">About</Link>
